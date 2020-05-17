5aba8896ff15048b9c743197b83ec6b96bb0c6d7
Carlos Felipe <develop.krlosflip@gmail.com>
2020-05-17 16:03:14 -0500

    Added first example "Hello world"

---
+++ 001HelloWorld.cpp
@@ -0,0 +1,13 @@
+/*
+Challenge name: Say "Hello, World!" With C++
+URL: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
